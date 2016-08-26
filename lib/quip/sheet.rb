a994bbcdc465e5c03a5d6af9ff80517842288a02
Eric Vautour <eric.vautour@kineticsocial.com>
2016-08-26 11:46:27 -0400

    Renamed methods

--- lib/quip/sheet.rb
+++ lib/quip/sheet.rb
@@ -7,26 +7,20 @@ module Quip
       super
     end
     
-    # Returns the text of items in the given row `ElementTree`.
-    def get_row_items(nodes)
-      nodes.collect{|n| n.text.delete("\n")}
-    end
-    
     # Returns the header row in the given quip_spreadsheet
-    def get_header_items
+    def header_items
       get_row_items(quip_sheet.at_css("tr").children)
     end
     
     # Dehumanize
-    def get_header_keys
+    def header_keys
       get_row_items(quip_sheet.at_css("tr").children).collect{|n|
         n.to_s.dup.downcase.gsub(/ +/,'_')
       }
     end
     
     # Return rows
-    def get_rows(options = {})
-      header_keys = get_header_keys
+    def rows(options = {})
       [].tap{|a|
         quip_sheet.css("tr").each_with_index do |row, i|
           _is_header = (i == 0)
@@ -53,7 +47,7 @@ module Quip
     # value 
     # sheet.find_row_by_value('fb_ad_campaign_id', '1122333')
     def find_row_by_value(key, value)
-      get_rows.each do |r|
+      rows.each do |r|
         if r.columns[key] && r.columns[key].text == value
           return r
         end
@@ -64,7 +58,14 @@ module Quip
     
     # Get row by index
     def get_row_by_index(index)
-      get_rows[index]
+      rows[index]
+    end
+    
+    private
+    
+    # Returns the text of items in the given row `ElementTree`.
+    def get_row_items(nodes)
+      nodes.collect{|n| n.text.delete("\n")}
     end
   end
 end
